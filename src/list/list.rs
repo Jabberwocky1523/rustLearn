402acdd75affb9c05610c769531826bdc68c69fc
Jabberwocky <wangy5236@gmail.com>
2025-02-19 11:48:19 +0800

    new file:   src/list/list.rs 	new file:   src/list/mod.rs 	modified:   src/main.rs 	new file:   src/test.rs

---
+++ src/list/list.rs
@@ -0,0 +1,29 @@
+pub mod LinkList {
+    use std::{boxed, option, os::unix::net::UnixDatagram, ptr::null, str::Lines};
+    struct LinkNode {
+        data: i32,
+        next: Option<Box<LinkNode>>,
+    }
+    pub struct LinkList {
+        head: Option<Box<LinkNode>>,
+    }
+    impl LinkList {
+        pub fn new() -> Self {
+            return LinkList { head: None };
+        }
+        pub fn push(&mut self, data: i32) {
+            let mut newnode = Box::new(LinkNode {
+                data: data,
+                next: self.head.take(),
+            });
+            self.head = Some(newnode);
+        }
+        pub fn print(&self) {
+            let mut current = &self.head;
+            while let Some(node) = current {
+                print!("{}", node.data);
+                current = &node.next;
+            }
+        }
+    }
+}
